c3287cc42fbe3061a2401776469185f1a11275aa
Prince Oncada <prince.oncada@gmail.com>
2024-07-03 13:26:35 -0400

    cta.tsx issue with apos

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -6,7 +6,7 @@ export default function Cta() {
             backgroundSize: "120%",
             backgroundPosition: "50% 15%",
         }}>
-            <h1 className="text-2xl font-bold xl:text-[56px]">Kickstart your child's journey</h1>
+            <h1 className="text-2xl font-bold xl:text-[56px]">Kickstart your child&apos;s journey</h1>
             <button className="bg-[#D2A808] text-lg font-medium xl:py-[18px] xl:px-[48px] py-2 px-4 xl:text-[32px]">Join us</button>
         </div>
     );
